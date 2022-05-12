abdbf9ec1bd3dbea58fff6f0ce748ab2626a1f48
Daniel Bernal <danielbernal85@gmail.com>
2022-05-12 11:54:42 -0300

    corrections on ex02

--- Understanding-java-methods/ex02/src/app/App.java
+++ Understanding-java-methods/ex02/src/app/App.java
@@ -1,3 +1,4 @@
+package app;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,20 +13,20 @@ public class App {
         System.out.println("SQUARE:");
         System.out.print("Enter the side of the square: ");
         double side = input.nextDouble();
-        Quadrilaterals.areaSquare(side);
+        Quadrilaterals.area(side);
 
         System.out.println("\nRECTANGLE:");
         System.out.print("Enter the base and the height of the rectangle: ");
         double base = input.nextDouble();
         double height = input.nextDouble();
-        Quadrilaterals.areaRectangle(base, height);
+        Quadrilaterals.area(base, height);
 
         System.out.println("\nTRAPEZIUM:");
         System.out.print("Enter the two bases and the height of the trapezium: ");
-        double base1 = input.nextDouble();
-        double base2 = input.nextDouble();
-        height = input.nextDouble();
-        Quadrilaterals.areaTrapezium(base1, base2, height);
+        float base1 = input.nextFloat();
+        float base2 = input.nextFloat();
+        float height1 = input.nextFloat();
+        Quadrilaterals.area(base1, base2, height1);
 
         input.close();
     }
--- Understanding-java-methods/ex02/src/entities/Quadrilaterals.java
+++ Understanding-java-methods/ex02/src/entities/Quadrilaterals.java
@@ -2,18 +2,19 @@ package entities;
 
 public class Quadrilaterals {
 
-    public static void areaSquare (double side) {
+    public static void area(double side) {
         double area = side * 2;
         System.out.println("The area of the square is: " + area);
     }
 
-    public static void areaRectangle (double base, double height) {
+    public static void area (double base, double height) {
         double area = base * height;
         System.out.println("The area of the rectangle is: " + area);
     }
 
-    public static void areaTrapezium (double base1, double base2, double height) {
+    public static void area (float base1, float base2, float height) {
         double area = ((base1 + base2) * height) / 2;
         System.out.println("The area of the trapezium is: " + area);
     }
+
 }
