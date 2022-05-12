63a102fdeeddc126483e0019259f275af0ab852b
Daniel Bernal <danielbernal85@gmail.com>
2022-05-12 18:50:43 -0300

    Separated goals in different methods

--- Conditional-and-flow-control/ex02/src/App.java
+++ Conditional-and-flow-control/ex02/src/App.java
@@ -5,9 +5,44 @@ public class App {
 
         Scanner input = new Scanner (System.in);
 
+        System.out.print("Which day of the week is it? ");
+        String dayOfTheWeek = input.next();
+        numDayWeek(dayOfTheWeek);
+        
         System.out.print("What's the variable value? ");
-        int a = input.nextInt();
+        int a = input.nextInt();    
+        definedVariable(a);
+        
+        input.close();
+    }
+    
+    public static void numDayWeek(String dayOfTheWeek) {
+        switch (dayOfTheWeek) {
+            case "sunday": 
+            System.out.println("1");
+            break;
+            case "monday": 
+                System.out.println("2");
+                break;
+            case "tuesday": 
+                System.out.println("3");
+                break;
+            case "wednesday": 
+                System.out.println("4");
+                break;
+            case "thursday": 
+                System.out.println("5");
+                break;
+            case "friday": 
+                System.out.println("6");
+                break;
+            case "saturday": 
+                System.out.println("7");
+                break;
+        }
+    }        
 
+    public static void definedVariable(int a) {
         switch (a) {
             case 1:
                 System.out.println("RIGHT!");
@@ -27,7 +62,6 @@ public class App {
             default:
                 System.out.println("UNDEFINED");
         }
-
-        input.close();
+                
     }
 }
