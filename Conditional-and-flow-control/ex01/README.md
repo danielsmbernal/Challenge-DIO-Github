ed38448b3d3d89063bd82f8c30f3ae0ff84ff2a0
Daniel Bernal <danielbernal85@gmail.com>
2022-05-12 17:55:03 -0300

    Added ex01

---
+++ Conditional-and-flow-control/ex01/README.md
@@ -0,0 +1,20 @@
+# **Exercise number 01** :metal: :metal: :metal:
+
+## :oncoming_automobile: **Goals**:  
+
+:white_check_mark: With if, display the name of the month according to its number.
+
+:white_check_mark: Create an if that checks if the month is july, december or january, in order to display the text "vacation".
+
+
+:no_entry: Rules
+
+* Avoid multiple if statements.
+  
+<br>
+
+## AND THAT'S IT!!!
+
+<br>
+
+##### ***PS: don't forget to drink water and eat ramen*** :ramen: :ramen: :ramen:
---
+++ Conditional-and-flow-control/ex01/src/App.java
@@ -0,0 +1,43 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) {
+
+        Date currentDate = new Date();
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(currentDate);
+        int month = 1 + cal.get(Calendar.MONTH);
+
+        if (month == 1) {
+            System.out.println("JANUARY");
+        } else if( month == 2){
+            System.out.println("FEBRUARY");
+        } else if( month == 3){
+            System.out.println("MARCH");
+        } else if( month == 4){
+            System.out.println("APRIL");
+        } else if( month == 5){
+            System.out.println("MAY");
+        } else if( month == 6){
+            System.out.println("JUNE");
+        } else if( month == 7){
+            System.out.println("JULY");
+        } else if( month == 8){
+            System.out.println("AUGUST");
+        } else if( month == 9){
+            System.out.println("SEPTEMBER");
+        } else if( month == 10){
+            System.out.println("OCTOBER");
+        } else if( month == 11){
+            System.out.println("NOVEMBER");
+        } else {
+            System.out.println("DECEMBER");
+        }
+
+        if (month == 1 || month == 7 || month == 12 ) {
+            System.out.println("VACATION!!!");
+        }   
+    }
+}
